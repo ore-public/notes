865cd6d66e46bc33f186bcc6bb6bb924456ba981
Masaya Konishi <konishi@office-ore.net>
2020-11-24 16:30:42 +0900

    メニューにえいごを追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">の〜と</router-link> |
-      <router-link to="/signin">ろぐいん</router-link> |
-      <router-link to="/about">あばうと</router-link>
+      <router-link to="/">の〜と(Private Note)</router-link> |
+      <router-link to="/signin">ろぐいん(Signin)</router-link> |
+      <router-link to="/about">あばうと(About)</router-link>
     </div>
     <router-view/>
   </div>
